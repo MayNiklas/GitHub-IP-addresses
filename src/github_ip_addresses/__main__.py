f5f4d72d58f683927c4047b799dabc4c1b7ad8c5
MayNiklas <info@niklas-steffen.de>
2022-03-23 15:35:14 +0000

    split code

--- src/github_ip_addresses/__main__.py
+++ src/github_ip_addresses/__main__.py
@@ -3,24 +3,32 @@ import json
 import requests
 
 
+def get(json_data,list):
+    result = []
+    for i in json_data[list]:
+        result.append(i)
+    return(result)
+
 def writer(json_data, lists):
+
+    if not os.path.exists('lists/'):
+        os.mkdir('lists/')
+
     for l in lists:
+        result = get(json_data,l)
         f = open("lists/" + l + ".txt", "a")
-
-        for i in json_data[l]:
+        for i in result:
             f.write(i + "\n")
+            print(i)
         f.close()
-
 
 def main():
     api_url = "https://api.github.com/meta"
     lists = ["hooks", "web", "api", "git", "packages", "pages", "importer", "actions", "dependabot"]
 
-    if not os.path.exists('lists/'):
-        os.mkdir('lists/')
-
     response = requests.get(api_url)
     json_data = json.loads(response.text)
+    
     writer(json_data, lists)
 
 
